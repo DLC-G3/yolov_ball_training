e6fc1db404686f7675c6f04d4e67aaed073e3069
Devreese Jorik <jorik.devreese@student.howest.be>
2022-06-06 16:56:35 +0200

    detects goals automatically

--- DataHandler/GoalDetection.py
+++ DataHandler/GoalDetection.py
@@ -1,5 +1,25 @@
 class GoalDetection():
     @staticmethod
-    def detect_goals_from_json(data,consecutive_frames=3,wait_interval=100):
+    def detect_goals_from_json(data,consecutive_frames=3,wait_interval=100,suffix="frame",extension=".jpg",consecutive_th=2):
+        consecutive_balls = 1
+        prev_frame = 0
+        check_delay = 0
         for img,data in data.items():
-            print(f"{img}, {data}")
+            for obj in data:
+                frame = int(img.replace(suffix,"").replace(extension,""))
+                class_ = obj["class"]
+                coords = [round(coord,2) for coord in obj["coords"]]
+                confidence = round(obj["confidence"]*100,2)
+                print(f"{frame}: {class_}, {coords}, {confidence} % confident")
+
+                if class_ == "Ball":
+                    if frame < prev_frame+5: # small range if detection isn't perfect
+                        consecutive_balls += 1
+                        if consecutive_balls >= consecutive_th:
+                            if check_delay == 0:
+                                print(f"Placing flag at {frame}")
+                                check_delay = 200+1 # delay for frames not to put flag
+
+            consecutive_balls = 1
+            check_delay = max(0,check_delay-1)
+            prev_frame = frame
--- main.py
+++ main.py
@@ -7,11 +7,10 @@ if __name__ == '__main__':
     # requestHandler = RequestHandler()
     # requestHandler.download_video_by_recording_url("")
 
-    VideoHandler.get_all_cropped_images("videoclip_977455",4)
-
-    #detected_balls = detect.run_optimised_for_ball(source="training_data/data/images/test/",weights="trained_data/latest_/weights/last.pt",imgsz=(416),conf_thres=(0.5),save_txt=True,project="./ball_output")  
-    # print(json_)
-    #detected_goals = GoalDetection.detect_goals_from_json(detected_balls)
-    #requestHandler.send_goals(deteced_goals,cam=4)
+    #VideoHandler.get_all_cropped_images("videoclip_977455",4)
+    detected_balls = detect.run_optimised_for_ball(source="DataHandler/SourceFiles/Images/",weights="trained_data/latest_/weights/last.pt",imgsz=(416),conf_thres=(0.06),save_txt=True,project="./ball_output")  
+    #print(json_)
+    detected_goals = GoalDetection.detect_goals_from_json(detected_balls)
+    #requestHandler.send_goals(detected_goals,cam=4)
     
 
